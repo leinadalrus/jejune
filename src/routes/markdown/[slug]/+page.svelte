6fb7c3c6023d5057e435d1477f29ff31bcc865ce
Daniel David Sansano Surla <dandavesan@gmail.com>
2022-12-10 10:30:01 +1100

    updated rust packages/targets

---
+++ src/routes/markdown/[slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+  import type { PageData } from './$types.d.ts'
+  import ContentBody from '../../../app/components/ContentBody/ContentBody.svelte'
+  import EditorLayout from '../../../layouts/EditorLayout.svelte'
+  export let data: PageData
+</script>
+
+<ContentBody>
+  <EditorLayout>
+    <p>{data.title}</p>
+    <div>{@html data.content}</div>
+  </EditorLayout>
+</ContentBody>
